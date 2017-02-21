d5740653a865684f364d373ada27a1516ce076cd
songzenglin <songzenglin@guazi.com>
2017-02-21 17:03:24 +0800

    lanse

--- scripts/demo.php
+++ scripts/demo.php
@@ -9,6 +9,16 @@ require_once dirname(__DIR__) . '/bootstrap.php';
 
 use \ROOT\Library\Model\TypicmsMenusModel;
 use \ROOT\Library\Util\PinyinUtil;
+use \ROOT\Library\Util\Client\CommClient;
+
+$appInfo = [];
+$sendParam = [];
+$client = new CommClient($appInfo['key'], $appInfo['secret'],[
+    'base_uri' => 'http://call.guazi.com/internal/',
+    'timeout'  => 5.0,
+    'verify'   => false
+]);
+$returnData = $client->get('call_sound_record/find', $sendParam);
 
 $list = TypicmsMenusModel::getInstance()->getList('*','1=1');
 var_dump($list);
